52c08daed30a1971f8511af6cbf97e7df9d799cb
Lucas da Paz <xlucaspx@gmail.com>
2022-12-06 19:17:11 -0300

    update - aula 2

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-console */
-// import assert from 'node:assert/strict';
-
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
@@ -9,36 +6,3 @@ export {
   somaHorasExtras,
   calculaDescontos,
 };
-
-// const verifiqueSe = (valor) => {
-//   const assercoes = {
-//     ehExatamenteIgualA(valorEsperado) {
-//       assert.strictEqual(valor, valorEsperado);
-//     },
-//   };
-
-//   return assercoes;
-// };
-
-// const teste = (titulo, funcaoDeTeste) => {
-//   try {
-//     funcaoDeTeste();
-//     console.log(`${titulo} passou!`);
-//   } catch (e) {
-//     console.error(`${titulo} não passou!`);
-//   }
-// };
-
-// teste('somaHorasExtras', () => {
-//   const valorEsperado = 2675;
-//   const retorno = somaHorasExtras(2500, 175);
-
-//   verifiqueSe(retorno).ehExatamenteIgualA(valorEsperado);
-// });
-
-// teste('calculaDesconto', () => {
-//   const valorEsperado = 2850;
-//   const retorno = calculaDescontos(3000, 150);
-
-//   verifiqueSe(retorno).ehExatamenteIgualA(valorEsperado);
-// });
--- test/folhaDePagamento.test.js
+++ test/folhaDePagamento.test.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-undef */
+/* eslint-disable no-console */
+// import assert from 'node:assert/strict';
+
 import { calculaDescontos, somaHorasExtras } from '..';
 
 describe('Testes dos cálculos de folha', () => {
@@ -17,3 +20,38 @@ describe('Testes dos cálculos de folha', () => {
     expect(retorno).toBe(valorEsperado);
   });
 });
+
+// Estrutura de testes construída do zero:
+
+// const verifiqueSe = (valor) => {
+//   const assercoes = {
+//     ehExatamenteIgualA(valorEsperado) {
+//       assert.strictEqual(valor, valorEsperado);
+//     },
+//   };
+
+//   return assercoes;
+// };
+
+// const teste = (titulo, funcaoDeTeste) => {
+//   try {
+//     funcaoDeTeste();
+//     console.log(`${titulo} passou!`);
+//   } catch (e) {
+//     console.error(`${titulo} não passou!`);
+//   }
+// };
+
+// teste('somaHorasExtras', () => {
+//   const valorEsperado = 2675;
+//   const retorno = somaHorasExtras(2500, 175);
+
+//   verifiqueSe(retorno).ehExatamenteIgualA(valorEsperado);
+// });
+
+// teste('calculaDesconto', () => {
+//   const valorEsperado = 2850;
+//   const retorno = calculaDescontos(3000, 150);
+
+//   verifiqueSe(retorno).ehExatamenteIgualA(valorEsperado);
+// });
